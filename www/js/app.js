90f34666ed46fb256f3a69f5d17a5dba12df2dc2
Andy Tsai <andytsai14@gmail.com>
2015-09-20 03:48:46 -0400

    jk no home page

--- www/js/app.js
+++ www/js/app.js
@@ -278,14 +278,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngOpenFB', 'ngCordov
         }
     }
    })
-  .state('app.home', {
-    url: "/home",
-    views: {
-        'menuContent': {
-            templateUrl: "templates/home.html",
-        }
-    }
-   })
   ;
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/map');
